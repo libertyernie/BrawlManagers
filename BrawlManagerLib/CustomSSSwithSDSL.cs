6c297ab3dab3dfb28f23951a45857b10316ff460
libertyernie <libertyernie0@gmail.com>
2014-02-04 12:45:49 -0800

    Add comment

--- BrawlManagerLib/CustomSSSwithSDSL.cs
+++ BrawlManagerLib/CustomSSSwithSDSL.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 
 namespace BrawlManagerLib {
+    /// <summary>
+    /// Allows read-write access to Custom SSS code, and read-only access to all Stage-Dependent Song Loader instances.
+    /// This class will store the entire codeset, split between itself (the SSS code) and the portions before/after (in raw byte[]).
+    /// </summary>
 	public class CustomSSSwithSDSL : CustomSSS {
 		public CustomSSSwithSDSL(string[] s) : base(s) {
 			SongsByStage = SDSLScanner.SongsByStage(this);
