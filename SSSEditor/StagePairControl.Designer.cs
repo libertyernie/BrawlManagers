f6158c0a800e42eef5c37655b22d8a069aaf61d8
libertyernie <maybeway36@gmail.com>
2014-11-30 21:55:50 -0600

    Stretch/shrink SSS fronstsnames to appropriate size

--- SSSEditor/StagePairControl.Designer.cs
+++ SSSEditor/StagePairControl.Designer.cs
@@ -238,6 +238,7 @@
 			this.pictureBox1.Location = new System.Drawing.Point(352, 0);
 			this.pictureBox1.Name = "pictureBox1";
 			this.pictureBox1.Size = new System.Drawing.Size(208, 62);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
 			this.pictureBox1.TabIndex = 10;
 			this.pictureBox1.TabStop = false;
 			// 
