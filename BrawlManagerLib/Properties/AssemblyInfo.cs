2a6dbeb260a69b79b32b3da1a30f354761fc23b2
libertyernie <maybeway36@gmail.com>
2016-02-06 15:33:32 -0500

    Update version number to 3.7

--- BrawlManagerLib/Properties/AssemblyInfo.cs
+++ BrawlManagerLib/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BrawlManagerLib")]
-[assembly: AssemblyDescription("Shared library for libertyernie's Smahs Bros. tools")]
+[assembly: AssemblyDescription("Shared library for libertyernie's Smash Bros. tools")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BrawlManagerLib")]
-[assembly: AssemblyCopyright("Copyright © 2013-2015 libertyernie")]
+[assembly: AssemblyCopyright("Copyright © 2013-2016 libertyernie")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.6.0.0")]
-[assembly: AssemblyFileVersion("3.6.0.0")]
+[assembly: AssemblyVersion("3.7.0.0")]
+[assembly: AssemblyFileVersion("3.7.0.0")]
